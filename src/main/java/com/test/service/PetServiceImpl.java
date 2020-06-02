f5b5bee68528d6e32e448359a783c1f0b54de794
kimhyeongkyun <hyeogkyunkim03142@gmail.com>
2020-06-02 23:24:57 +0900

    cheong-a_fix

--- src/main/java/com/test/service/PetServiceImpl.java
+++ src/main/java/com/test/service/PetServiceImpl.java
@@ -19,56 +19,51 @@ import com.test.dto.CustomerDTO;
 import com.test.dto.PetDTO;
 
 @Service
-@SessionAttributes({ "customer", "company" })	// Model에 저장한 값을 http session에 저장할 수 있게 해주는 Annotation
-public class PetServiceImpl implements PetService{
-	
+@SessionAttributes({ "customer", "company" }) // Model에 저장한 값을 http session에 저장할 수 있게 해주는 Annotation
+public class PetServiceImpl implements PetService {
+
 	@Autowired
 	private PetDAO petDao;
 
 	@Override
-	public String modify(int cust_Index, int pet_Index, Model model,HttpServletRequest  request) {
-		Map<String,Integer> petMap = new HashMap<String,Integer>();		// 넘어온 변수를 한 번에 저장하기 위해서 만든 Map객체
-		petMap.put("customer_Index", cust_Index);						// Map객체에 customer_Index를 저장한다.
-		petMap.put("pet_Index", pet_Index);								// Map객체에 pet_Index를 저장한다.
-		
-		request.getSession().setAttribute("pet_Index", pet_Index);
-		
-		PetDTO pt= this.petDao.read(petMap);							// customer_Index, pet_Index에 해당하는 펫 정보를 가져온다.
-		model.addAttribute("pet",pt);									// 가져온 펫 정보를 model객체에 저장한다.
+	public String modify(int cust_Index, int pet_Index, Model model, HttpServletRequest request) {
+		Map<String, Integer> petMap = new HashMap<String, Integer>(); // 넘어온 변수를 한 번에 저장하기 위해서 만든 Map객체
+		petMap.put("customer_Index", cust_Index); // Map객체에 customer_Index를 저장한다.
+		petMap.put("pet_Index", pet_Index); // Map객체에 pet_Index를 저장한다.
+
+		PetDTO pt = this.petDao.read(petMap); // customer_Index, pet_Index에 해당하는 펫 정보를 가져온다.
+		model.addAttribute("pet", pt); // 가져온 펫 정보를 model객체에 저장한다.
 		return "pet/pet_modify.tiles";
 	}
 
 	@Override
-	public String postModify(PetDTO pet,HttpServletRequest  request) {
-		HttpSession session = request.getSession();														// Session을 가져온다.
-		
-		PetDTO pet2 = pet;
-		pet2.setCustomer_Index(((CustomerDTO) session.getAttribute("customer")).getCustomer_Index());
-		pet2.setPet_Index((Integer) session.getAttribute("pet_Index"));		
-		
-		this.petDao.update(pet2);							// 펫 정보를 수정하고 수정하기 버튼을 누르면 form에 입력한 값들로 기존 펫 정보를 udpate한다.
-		return "pet/pet_modify_ok.tiles";	// 펫 정보 수정이 끝나고 난 후 확인페이지로 이동
+	public String postModify(PetDTO pet, HttpServletRequest request) {
+
+		this.petDao.update(pet); // 펫 정보를 수정하고 수정하기 버튼을 누르면 form에 입력한 값들로 기존 펫 정보를 udpate한다.
+		return "pet/pet_modify_ok.tiles"; // 펫 정보 수정이 끝나고 난 후 확인페이지로 이동
 	}
 
 	@Override
 	public String reserve_Ok(HashMap<String, Object> pmap, HttpServletRequest request) {
-		HttpSession session = request.getSession();														// Session을 가져온다.
-		CustomerDTO customer = (CustomerDTO) session.getAttribute("customer");							// 가져온 Session에서 customer session을 가져와서 DTO로 타입캐스팅을 하고 customer에 저장한다.
-		int customer_Index = customer.getCustomer_Index();												// customer에서 index값만 따로 int변수에 저장한다.
-		pmap.put("customer_Index", customer_Index);														// HashMap객체에 form값 + custoer_Index를 저장한다.
-		this.petDao.insertThePet(pmap);																	// pet테이블에다가 값을 insert한다.
+		HttpSession session = request.getSession(); // Session을 가져온다.
+		CustomerDTO customer = (CustomerDTO) session.getAttribute("customer"); // 가져온 Session에서 customer session을 가져와서
+																				// DTO로 타입캐스팅을 하고 customer에 저장한다.
+		int customer_Index = customer.getCustomer_Index(); // customer에서 index값만 따로 int변수에 저장한다.
+		pmap.put("customer_Index", customer_Index); // HashMap객체에 form값 + custoer_Index를 저장한다.
+		this.petDao.insertThePet(pmap); // pet테이블에다가 값을 insert한다.
 		return "pet/pet_ok.tiles";
 	}
 
 	@Override
 	public String petcheck(Model model, HttpSession session) {
-		CustomerDTO customer = (CustomerDTO) session.getAttribute("customer");	// 가져온 Session에서 customer session을 가져와서 DTO로 타입캐스팅을 하고 customer에 저장한다.
-		int customer_Index = customer.getCustomer_Index();						// customer에서 index값만 따로 int변수에 저장한다.
-		List<PetDTO> itsPets = new ArrayList<PetDTO>();							// pet정보를 가져와서 저장할 List객체를 생성한다.
-		List<PetDTO> temp = this.petDao.listItsPets(customer_Index);			// customer_Index에 해당하는 모든 펫 정보를 pet테이블에서 전부 가져온다.
-		itsPets.addAll(temp);													// itsPets에 저장한다.
-		model.addAttribute("pet", itsPets);										// model객체에 모든 pet정보를 저장한다.
-		return "pet/pet_check.tiles";  
+		CustomerDTO customer = (CustomerDTO) session.getAttribute("customer"); // 가져온 Session에서 customer session을 가져와서
+																				// DTO로 타입캐스팅을 하고 customer에 저장한다.
+		int customer_Index = customer.getCustomer_Index(); // customer에서 index값만 따로 int변수에 저장한다.
+		List<PetDTO> itsPets = new ArrayList<PetDTO>(); // pet정보를 가져와서 저장할 List객체를 생성한다.
+		List<PetDTO> temp = this.petDao.listItsPets(customer_Index); // customer_Index에 해당하는 모든 펫 정보를 pet테이블에서 전부 가져온다.
+		itsPets.addAll(temp); // itsPets에 저장한다.
+		model.addAttribute("pet", itsPets); // model객체에 모든 pet정보를 저장한다.
+		return "pet/pet_check.tiles";
 	}
 
 	@Override
--- src/main/webapp/WEB-INF/views/pet/pet_modify.jsp
+++ src/main/webapp/WEB-INF/views/pet/pet_modify.jsp
@@ -7,59 +7,57 @@
 
 
 		<form action="pet_modify" class="pet_modify" name="modify" method="post">
+			<input type="hidden" name="customer_Index" value="${pet.customer_Index }">
+			<input type="hidden" name="pet_Index" value="${pet.pet_Index}">
 			<div class="form-group row">
 				<label class="col-sm-2 " for="pet_Name">이름</label>
 				<div class="col-sm-8">
-					<input class="form-control" type="text" id="pet_Name"
-						name="pet_Name" value="${pet.pet_Name }">
+					<input class="form-control" type="text" id="pet_Name" name="pet_Name" value="${pet.pet_Name }">
 				</div>
 			</div>
 			<div class="form-group row">
 				<label class="col-sm-2 col-form-label" for="pet_Type">종</label>
 				<div class="col-sm-8">
-					<input class="form-control" type="text" id="pet_Type"
-						name="pet_Type" value="${pet.pet_Type }">
+					<input class="form-control" type="text" id="pet_Type" name="pet_Type" value="${pet.pet_Type }">
 				</div>
 			</div>
 			<div class="form-group row">
 				<label class=" col-sm-2 col-form-label" for="pet_Gender">성별</label>
 				<div class="gender-radio-group col-sm-8">
-					<input type="radio" name="pet_Gender" value="암컷"
-						<c:if test="${pet.pet_Gender eq '암컷'}">checked="checked"</c:if>>암컷
-					<input type="radio" name="pet_Gender" value="수컷"
-						<c:if test="${pet.pet_Gender eq '수컷'}">checked="checked"</c:if>>수컷
+					<input type="radio" name="pet_Gender" value="암컷" <c:if
+						test="${pet.pet_Gender eq '암컷'}">checked="checked"</c:if>>암컷
+					<input type="radio" name="pet_Gender" value="수컷" <c:if
+						test="${pet.pet_Gender eq '수컷'}">checked="checked"</c:if>>수컷
 				</div>
 			</div>
 			<div class="form-group row">
 				<label class="col-sm-2 col-form-label" for="pet_Age">나이(개월수)</label>
 				<div class="col-sm-8">
-					<input class="form-control" type="number" id="pet_Age"
-						name="pet_Age" value="${pet.pet_Age }">
+					<input class="form-control" type="number" id="pet_Age" name="pet_Age" value="${pet.pet_Age }">
 				</div>
 			</div>
 			<div class="form-group row">
 				<label class="col-sm-2 col-form-label" for="pet_IsNeutralized">중성화수술</label>
 				<div class="gender-radio-group col-sm-8" id="pet_IsNeutralized">
-					<input type="radio" name="pet_IsNeutralized" value="1"
-						<c:if test="${pet.pet_IsNeutralized eq true}">checked="checked"</c:if>>
-					<i class="far fa-circle"></i> <input type="radio"
-						name="pet_IsNeutralized" value="0"
-						<c:if test="${pet.pet_IsNeutralized eq false}">checked="checked"</c:if>>
+					<input type="radio" name="pet_IsNeutralized" value="1" <c:if
+						test="${pet.pet_IsNeutralized eq true}">checked="checked"</c:if>>
+					<i class="far fa-circle"></i> <input type="radio" name="pet_IsNeutralized" value="0" <c:if
+						test="${pet.pet_IsNeutralized eq false}">checked="checked"</c:if>>
 					<i class="far fa-times-circle"></i>
 				</div>
 			</div>
 			<div class="form-group row">
 				<label class="col-sm-2 col-form-label" for="pet_Weight">무게</label>
 				<div class="col-sm-8">
-					<input type="number" class="form-control" id="pet_Weight"
-						placeholder="kg" name="pet_Weight" value="${pet.pet_Weight}">
+					<input type="number" class="form-control" id="pet_Weight" placeholder="kg" name="pet_Weight"
+						value="${pet.pet_Weight}">
 				</div>
 			</div>
 			<div class="form-group row">
 				<label class="col-sm-2 col-form-label" for="pet_History">기타사항</label>
 				<div class="col-sm-8">
-					<input class="form-control" type="text" id="pet_History"
-						name="pet_History" value="${pet.pet_History }">
+					<input class="form-control" type="text" id="pet_History" name="pet_History"
+						value="${pet.pet_History }">
 				</div>
 			</div>
 
