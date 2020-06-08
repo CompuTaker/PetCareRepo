5fbf347514e61466da751c711039f48b7888ae55
DESKTOP-6HO4AK1\ghaga <ironypj@gmail.com>
2020-06-08 22:03:26 +0900

    new redis public ip address

--- src/main/java/com/test/constants/Constants.java
+++ src/main/java/com/test/constants/Constants.java
@@ -10,7 +10,8 @@ public class Constants {
 	public static String IDX = "idx";
 	public static String IS_ADMIN = "isAdmin";
 	
-	public static String REDIS_PUBLIC_IP_ADDRESS = "18.179.78.249";
+	public static String REDIS_PUBLIC_IP_ADDRESS = "3.113.91.127";
+	// public static String REDIS_PUBLIC_IP_ADDRESS = "18.179.78.249";
 	
 	public static int REDIS_PUBLIC_PORT = 6379;
 	
